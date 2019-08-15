338f06d05729976c202dd2cadf2e1607f3f0484b
Mathieu Mence <Mathieu.Mence@matnard.com>
2019-08-16 07:15:12 +1000

    updates demo with nargl pkg

--- dev/index.js
+++ dev/index.js
@@ -1,56 +1,57 @@
 import * as NARD from "nardgl";
-import { Particles } from "./Particles";
-import { Quad } from "./Quad";
-import quadGltfUrl from "./Quad/quad.gltf";
-import { TexturedCube } from "./TexturedCube";
-import texturedCubeGltfUrl from "./TexturedCube/textured-cube.gltf";
-const urls = [quadGltfUrl, texturedCubeGltfUrl];
+debugger;
+//import { Particles } from "./Particles";
+//import { Quad } from "./Quad";
+//import quadGltfUrl from "./Quad/quad.gltf";
+//import { TexturedCube } from "./TexturedCube";
+//import texturedCubeGltfUrl from "./TexturedCube/textured-cube.gltf";
 
-class App extends NARD.WebGLRenderer {
-  init() {
-    this.particles = new Particles(this.gl);
-    this.scene.push(this.particles);
+// const urls = [quadGltfUrl, texturedCubeGltfUrl];
+// class App extends NARD.WebGLRenderer {
+//   init() {
+//     this.particles = new Particles(this.gl);
+//     this.scene.push(this.particles);
 
-    this.grid = new NARD.Grid(this.gl);
-    this.grid.rotation.x = Math.PI / 2;
-    this.scene.push(this.grid);
+//     this.grid = new NARD.Grid(this.gl);
+//     this.grid.rotation.x = Math.PI / 2;
+//     this.scene.push(this.grid);
 
-    this.cube = new TexturedCube(
-      this.gl,
-      new NARD.GltfParser(App.data[urls[1]]).getPrimitives()[0]
-    );
+//     this.cube = new TexturedCube(
+//       this.gl,
+//       new NARD.GltfParser(App.data[urls[1]]).getPrimitives()[0]
+//     );
 
-    this.cube.translation.y = 0.5;
-    this.cube.scale.x = this.cube.scale.y = this.cube.scale.z = 0.3;
+//     this.cube.translation.y = 0.5;
+//     this.cube.scale.x = this.cube.scale.y = this.cube.scale.z = 0.3;
 
-    this.scene.push(this.cube);
+//     this.scene.push(this.cube);
 
-    this.quad = new Quad(
-      this.gl,
-      new NARD.GltfParser(App.data[urls[0]]).getPrimitives()[0]
-    );
+//     this.quad = new Quad(
+//       this.gl,
+//       new NARD.GltfParser(App.data[urls[0]]).getPrimitives()[0]
+//     );
 
-    this.scene.push(this.quad);
+//     this.scene.push(this.quad);
 
-    this.camera.translation.z = 4;
-    this.camera.translation.y = 0.5;
-    // this.camera.rotation.x = Math.PI / 2;
-    this.render();
-  }
+//     this.camera.translation.z = 4;
+//     this.camera.translation.y = 0.5;
+//     // this.camera.rotation.x = Math.PI / 2;
+//     this.render();
+//   }
 
-  beforeDraw(dt) {
-    this.camera.rotation.y -= 0.005;
-  }
-}
+//   beforeDraw(dt) {
+//     this.camera.rotation.y -= 0.005;
+//   }
+// }
 
-new NARD.Loader({
-  onProgress: function(progress) {
-    console.log(`Progress: ${progress}`);
-  },
-  assets: urls
-})
-  .start()
-  .then(function(data) {
-    App.data = data;
-    new App();
-  });
+// new NARD.Loader({
+//   onProgress: function(progress) {
+//     console.log(`Progress: ${progress}`);
+//   },
+//   assets: urls
+// })
+//   .start()
+//   .then(function(data) {
+//     App.data = data;
+//     new App();
+//   });
