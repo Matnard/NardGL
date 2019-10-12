95e52b048ff910ecdbeacfaa7f4e58827b06e35e
Mathieu Mence <Mathieu.Mence@matnard.com>
2019-10-12 23:10:30 +1100

    adds wasd to controls

--- packages/camera-control/lib/camera-control.js
+++ packages/camera-control/lib/camera-control.js
@@ -17,6 +17,11 @@ class CameraControl {
     this.downPressed = false;
     this.upPressed = false;
 
+    this.dDown = false;
+    this.aDown = false;
+    this.sDown = false;
+    this.wDown = false;
+
     this.deltaXSign = 0;
     this.deltaYSign = 0;
 
@@ -43,6 +48,18 @@ class CameraControl {
     } else if (keyCode == 38) {
       this.upPressed = true;
     }
+
+    if (keyCode == 68) {
+      this.dPressed = true;
+    } else if (keyCode == 65) {
+      this.aPressed = true;
+    }
+
+    if (keyCode == 83) {
+      this.sPressed = true;
+    } else if (keyCode == 87) {
+      this.wPressed = true;
+    }
   }
 
   onKeyUp({ keyCode }) {
@@ -57,31 +74,54 @@ class CameraControl {
     } else if (keyCode == 38) {
       this.upPressed = false;
     }
+
+    if (keyCode == 68) {
+      this.dPressed = false;
+    } else if (keyCode == 65) {
+      this.aPressed = false;
+    }
+
+    if (keyCode == 83) {
+      this.sPressed = false;
+    } else if (keyCode == 87) {
+      this.wPressed = false;
+    }
   }
 
   update() {
     if (this.rightPressed) {
-      this.camera.translation.x += 5;
+      this.camera.rotation.y -= Math.PI / 90;
     } else if (this.leftPressed) {
-      this.camera.translation.x -= 5;
+      this.camera.rotation.y += Math.PI / 90;
     }
     if (this.downPressed) {
-      this.camera.rotation.x += Math.PI / 30;
+      this.camera.rotation.x += Math.PI / 90;
     } else if (this.upPressed) {
-      this.camera.rotation.x -= Math.PI / 30;
+      this.camera.rotation.x -= Math.PI / 90;
     }
 
-    if (this.deltaXSign > 0) {
-      this.camera.rotation.y += Math.PI / 30;
-    } else if (this.deltaXSign < 0) {
-      this.camera.rotation.y -= Math.PI / 30;
+    if (this.dPressed) {
+      this.camera.translation.x += 0.1;
+    } else if (this.aPressed) {
+      this.camera.translation.x -= 0.1;
+    }
+    if (this.sPressed) {
+      this.camera.translation.z += 0.1;
+    } else if (this.wPressed) {
+      this.camera.translation.z -= 0.1;
     }
 
-    if (this.deltaYSign > 0) {
-      this.camera.translation.z += 5;
-    } else if (this.deltaYSign < 0) {
-      this.camera.translation.z -= 5;
-    }
+    // if (this.deltaXSign > 0) {
+    //   this.camera.rotation.y += Math.PI / 30;
+    // } else if (this.deltaXSign < 0) {
+    //   this.camera.rotation.y -= Math.PI / 30;
+    // }
+
+    // if (this.deltaYSign > 0) {
+    //   this.camera.translation.z += 5;
+    // } else if (this.deltaYSign < 0) {
+    //   this.camera.translation.z -= 5;
+    // }
   }
 }
 
