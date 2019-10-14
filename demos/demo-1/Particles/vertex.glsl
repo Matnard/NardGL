36528e4a67ec22f61209db54b0ceae7cce6c9371
Mathieu Mence <Mathieu.Mence@matnard.com>
2019-10-14 23:14:37 +1100

    scales particles

--- demos/demo-1/Particles/vertex.glsl
+++ demos/demo-1/Particles/vertex.glsl
@@ -1,7 +1,7 @@
 void main() {
-  gl_PointSize = 25.0;//u_pointSize;
   vec4 modelPosition = u_modelMatrix * vec4(POSITION, 1.0);
-
-  gl_Position = u_projectionMatrix * u_viewMatrix * modelPosition;
+  vec4 position = u_projectionMatrix * u_viewMatrix * modelPosition;
+  gl_Position = position;
+  gl_PointSize = 200.0/position.z;
   //gl_Position = modelViewPosition;
 }
