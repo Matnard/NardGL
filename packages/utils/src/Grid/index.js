45008e7900b573453136eecf0e3d3a927a9c9bcd
Mathieu Mence <Mathieu.Mence@matnard.com>
2019-10-31 06:51:43 +1100

    updates grid

--- packages/utils/src/Grid/index.js
+++ packages/utils/src/Grid/index.js
@@ -1,98 +1,80 @@
 import fragmentShaderPartial from "./fragment.glsl";
 import vertexShaderPartial from "./vertex.glsl";
-import { BasicMaterial, Primitive } from "@nardgl/core";
+import { Geometry, Vertex, CustomAttribute, Material, Primitive } from "nardgl";
 
-let srcData = [
-  0,
-  -1,
-  0,
-  3,
+const geometry = new Geometry(Geometry.SIMPLE_PARTITION);
 
-  0,
-  1,
-  0,
-  3,
-
-  0,
-  0,
-  -1,
-  2,
-
-  0,
-  0,
-  1,
-  2,
-
-  -1,
-  0,
-  0,
-  1,
-
-  1,
-  0,
-  0,
-  1
-];
 let subDiv = 14;
 let step = 2 / subDiv;
 
+const subs = [];
+
+const addToSubs = (x, y, z, colorIndex) => {
+  subs.push(
+    new Vertex(x, y, z, [
+      new CustomAttribute("POSITION", "VEC3", 5126, x, y, z),
+      new CustomAttribute("COLORS", "SCALAR", 5126, colorIndex)
+    ])
+  );
+};
+
 for (var i = step; i <= 1; i += step) {
-  let verticals = [i, 1, 0, 0, i, -1, 0, 0, -i, 1, 0, 0, -i, -1, 0, 0];
-  let horizontals = [1, i, 0, 0, -1, i, 0, 0, 1, -i, 0, 0, -1, -i, 0, 0];
-  srcData = [...srcData, ...verticals, ...horizontals];
+  addToSubs(i, 1, 0, 0);
+  addToSubs(i, -1, 0, 0);
+  addToSubs(-i, 1, 0, 0);
+  addToSubs(-i, -1, 0, 0);
+
+  addToSubs(1, i, 0, 0);
+  addToSubs(-1, i, 0, 0);
+  addToSubs(1, -i, 0, 0);
+  addToSubs(-1, -i, 0, 0);
 }
 
-const count = srcData.length / 4;
+geometry.setVertices([
+  new Vertex(0, -1, 0, [
+    new CustomAttribute("POSITION", "VEC3", 5126, 0, -1, 0),
+    new CustomAttribute("COLORS", "SCALAR", 5126, 3)
+  ]),
+  new Vertex(0, 1, 0, [
+    new CustomAttribute("POSITION", "VEC3", 5126, 0, 1, 0),
+    new CustomAttribute("COLORS", "SCALAR", 5126, 3)
+  ]),
+  new Vertex(0, 0, -1, [
+    new CustomAttribute("POSITION", "VEC3", 5126, 0, 0, -1),
+    new CustomAttribute("COLORS", "SCALAR", 5126, 2)
+  ]),
+  new Vertex(0, 0, 1, [
+    new CustomAttribute("POSITION", "VEC3", 5126, 0, 0, 1),
+    new CustomAttribute("COLORS", "SCALAR", 5126, 2)
+  ]),
+  new Vertex(-1, 0, 0, [
+    new CustomAttribute("POSITION", "VEC3", 5126, -1, 0, 0),
+    new CustomAttribute("COLORS", "SCALAR", 5126, 1)
+  ]),
+  new Vertex(1, 0, 0, [
+    new CustomAttribute("POSITION", "VEC3", 5126, 1, 0, 0),
+    new CustomAttribute("COLORS", "SCALAR", 5126, 1)
+  ]),
+  ...subs
+]);
 
-const attributesData = [
-  {
-    name: "a_position",
-    type: "VEC3",
-    componentType: 5126,
-    count,
-    srcData,
-    stride: Float32Array.BYTES_PER_ELEMENT * 4,
-    offset: 0
-  },
-  {
-    name: "a_colors",
-    type: "SCALAR",
-    componentType: 5126,
-    count,
-    stride: Float32Array.BYTES_PER_ELEMENT * 4,
-    offset: Float32Array.BYTES_PER_ELEMENT * 3
-  }
-];
-
-const uniformsData = [
-  {
-    name: "u_colors",
-    type: "3fv",
-    value: [0.8, 0.8, 0.8, 1, 0, 0, 0, 1, 0, 0, 0, 1],
-    count: 4
-  }
-];
-
-const material = new BasicMaterial(
-  attributesData,
-  uniformsData,
-  [],
-  vertexShaderPartial,
-  fragmentShaderPartial
-);
-
-const conf = {
-  material,
-  count,
-  draw: {
-    primitiveType: 1,
-    offset: 0
-  }
-};
+const material = new Material({
+  uniformsData: [
+    {
+      name: "u_colors",
+      type: "3fv",
+      value: [0.8, 0.8, 0.8, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+      count: 4
+    }
+  ],
+  primitiveType: Material.LINES,
+  vertexShader: vertexShaderPartial,
+  fragmentShader: fragmentShaderPartial
+});
 
 class Grid extends Primitive {
   constructor() {
-    super(conf);
+    super(geometry, material);
   }
 
   beforeDraw() {
--- packages/utils/src/Grid/vertex.glsl
+++ packages/utils/src/Grid/vertex.glsl
@@ -1,7 +1,7 @@
 out lowp vec4 color;	//Color to send to fragment shader.
 
 void main(void){
-
-	gl_Position = u_projectionMatrix * u_viewMatrix * u_modelMatrix * vec4(a_position, 1.0);
-	color = vec4(u_colors[ int(a_colors) ], 1.0); //Using the 4th float as a color index.
+	gl_PointSize = 25.0;
+	gl_Position = u_projectionMatrix * u_viewMatrix * u_modelMatrix * vec4(POSITION, 1.0);
+	color = vec4(u_colors[ int(COLORS) ], 1.0); //Using the 4th float as a color index.
 }
